e6d921fa3e0a193215a4933109a1b4c6cb0d6717
Sam MSI <spt7ponder@aol.com>
2022-08-28 18:25:13 -0700

    built the about me page

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+olddisplay = "TOVG"
+function switchDisplay(id){
+    document.getElementById(olddisplay).style.display = 'none'
+    document.getElementById(olddisplay+'Selector').style.backgroundColor = 'rgb(119, 189, 214)';
+    document.getElementById(id).style.display = 'block'
+    document.getElementById(id +'Selector').style.backgroundColor = 'rgb(190, 218, 228)'
+    olddisplay = id;
+}
